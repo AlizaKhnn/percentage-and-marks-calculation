ec63c8b91f8f1c5fcaa1886405d2ffabb7cd04cb
Aliza Khan <160646235+AlizaKhnn@users.noreply.github.com>
2024-04-21 14:51:40 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+var english = 90;
+var maths = 33;
+var science = 55;
+var totalMarks = 300;
+console.log("totalMarks: ", totalMarks);
+var obtainedMarks = english + maths + science;
+console.log("obtainedMarks: ", obtainedMarks);
+var percentage = obtainedMarks / totalMarks * 100;
+console.log('percentage: ', percentage);
---
+++ index.ts
@@ -0,0 +1,11 @@
+const english = 90;
+const maths = 33;
+const science = 55;
+
+const totalMarks = 300;
+console.log("totalMarks: ", totalMarks);
+const obtainedMarks = english + maths + science;
+
+console.log("obtainedMarks: ", obtainedMarks);
+const percentage = obtainedMarks / totalMarks *100;
+console.log('percentage: ', percentage);
